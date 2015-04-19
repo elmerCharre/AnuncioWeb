8b84020bb584397ac3de0c36c8dca09fa3138593
elmerCharre <elmer.nyd@gmail.com>
2015-04-19 12:19:19 -0500

    update config

---
+++ Ads/Ads/tokenProvider/MachineKeyProtectionProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Security;
+using Microsoft.Owin.Security.DataProtection;
+
+namespace Ads.tokenProvider
+{
+    public class MachineKeyProtectionProvider : IDataProtectionProvider
+    {
+        public IDataProtector Create(params string[] purposes)
+        {
+            return new MachineKeyDataProtector(purposes);
+        }
+    }
+
+    public class MachineKeyDataProtector : IDataProtector
+    {
+        private readonly string[] _purposes;
+
+        public MachineKeyDataProtector(string[] purposes)
+        {
+            _purposes = purposes;
+        }
+
+        public byte[] Protect(byte[] userData)
+        {
+            return MachineKey.Protect(userData, _purposes);
+        }
+
+        public byte[] Unprotect(byte[] protectedData)
+        {
+            return MachineKey.Protect(protectedData, _purposes);
+        }
+    }
+
+}
